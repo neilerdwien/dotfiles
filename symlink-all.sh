98dad8ff42daf61c196297467bba3a037caf2c28
Neil Erdwien <neil@k-state.edu>
2021-09-02 22:40:22 +0000

    Initial checkin

---
+++ symlink-all.sh
@@ -0,0 +1,10 @@
+# Bash
+ln -s ~/.dotfiles/.aws ~
+
+# Bash
+ln -s ~/.dotfiles/.bashrc ~
+ln -s ~/.dotfiles/.bash_logout ~
+ln -s ~/.dotfiles/.bash_profile ~
+
+# git
+ln -s ~/.dotfiles/.gitconfig ~
